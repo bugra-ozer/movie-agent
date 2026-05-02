40f10fa4c15a9507d90f7eb572116764c388f40d
git-bugra <bugraozer791@gmail.com>
2026-05-02 13:05:04 +0100

    fix: logging configured at info level

--- log/log_handler.py
+++ log/log_handler.py
@@ -9,7 +9,7 @@ class LogHandler():
         self.filename=filename
         self.path=pl.Path(__file__).parent.parent / directory / filename #Create full path
         pl.Path.mkdir(self.path.parent, parents=True, exist_ok=True) #Make directory for logging files
-        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         self.logger = logging.getLogger()
         self.handler=logging.handlers.RotatingFileHandler(self.path, maxBytes=10485760, backupCount=5)
         self.handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
